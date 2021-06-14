2213a9be2272f5d5a693b0089629bc7c32adc742
chandan-9606 <85636455+chandan-9606@users.noreply.github.com>
2021-06-14 07:34:48 -0700

    The File by fibonacci programming
    Developed By Chandan programming by fibonacci numbers

---
+++ abc.py
@@ -0,0 +1,10 @@
+#include<studio.h>
+
+n = int(input("enter the number:"))
+
+
+if 0*(n*n)+144 or 0*(n*n)-144:
+    print(n,"is fibonacci number")
+else:
+    print(n,"is not fibonacci number")
+    
